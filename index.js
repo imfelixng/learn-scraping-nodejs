8afa0978d2727a35ea46411ecc21aec239d1806d
Nguyen Quang An <ngquangan@gmail.com>
2019-01-23 22:29:55 +0700

    Simple get content website with request-promise and cheerio

---
+++ index.js
@@ -0,0 +1,14 @@
+const request = require('request-promise');
+const cheerio = require('cheerio');
+
+const URL = "https://www.imdb.com/title/tt1477834/";
+
+(async () => {
+  const response = await request(URL);
+  let $ = cheerio.load(response);
+
+  let titleMovie = $('.title_wrapper > h1').text();
+
+  console.log(titleMovie);
+
+})();
